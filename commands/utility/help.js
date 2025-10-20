308517c87194b4fd13b260e6e1e4972067679e06
VoidThreader <eizendragoon@gmail.com>
2025-10-20 22:24:01 +0800

    Help command updated for roll 	Added new features to roll

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription(`Provides information about the bot's commands`),
+		.setDescription(`Provides information about the bot's commands.`),
 	
 	async execute(interaction) {
 		const helpMessage = new EmbedBuilder()
@@ -15,7 +15,7 @@ module.exports = {
 				• **/flip**: Mystic flour cookie performs mystic flips.
 				• **/aurafarm**: Silent Salt aurafarming.
 				• **/rng**: Number guessing game, rolls a d20 on default. To quit, type "quit" in chat.
-				• **/roll**: Rolls a d20 on default. Change it with the lowest and highest options.
+				• **/roll**: Rolls a d20 on default. Change it with given options.
 				• **/8-ball**: Answers a question.
 				• **/shadowmilk**: Sends an image of shadow milk cookie.
 			`)
--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -5,7 +5,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
 		.setDescription(
-			'Rolls a d20 on default. Change it with the lowest and highest options.',
+			'Rolls a d20 on default. Change it with given options.',
 		)
 		.addIntegerOption(option =>
 			option.setName('lowest')
@@ -16,9 +16,22 @@ module.exports = {
 			option.setName('highest')
 				.setDescription('Highest number to roll.')
 				.setRequired(false),
+		)
+		.addIntegerOption(option =>
+			option.setName('rolls')
+				.setDescription('Number of dice rolls. Max of 100.')
+				.setRequired(false),
+		)
+		.addIntegerOption(option =>
+			option.setName('modifier')
+				.setDescription('Add or subtract to your roll.')
+				.setRequired(false),
 		),
 
 	async execute(interaction) {
+		
+		let sumRoll = 0;
+		let finalRoll = 1;
 
 		const range = {
 			lowest : interaction.options.getInteger('lowest') ?? 1,
@@ -29,7 +42,18 @@ module.exports = {
 			[range.lowest, range.highest] = [range.highest, range.lowest];
 		}
 
-		const generatedNum = Math.floor(Math.random() * (range.highest - range.lowest + 1)) + range.lowest;
-		await interaction.reply(`${generatedNum}`);
+		const rolls = Math.min(Math.max(interaction.options.getInteger('rolls') ?? 1, 1), 100);
+		for (let i = 0; i < rolls; i++) {
+			const generatedNum = Math.floor(Math.random() * (range.highest - range.lowest + 1)) + range.lowest;
+			sumRoll += generatedNum;
+		}
+
+		const modifier = interaction.options.getInteger('modifier') ?? 0;
+		
+		if ((sumRoll + modifier) > 1) {
+			finalRoll = sumRoll + modifier;
+		}
+		
+		await interaction.reply(`${finalRoll}`);
 	},
 };

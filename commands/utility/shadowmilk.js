b9a6f2895f538cb14c0c7e52a3d7d66ae592a949
VoidThreader <notvoidthreaderl@gmail.com>
2025-11-16 22:25:36 +0800

    Smilk randomized image output

--- commands/utility/shadowmilk.js
+++ commands/utility/shadowmilk.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { getRandInt } = require('../../utils/rand.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('shadowmilk')
 		.setDescription('Sends an image of shadow milk cookie.'),
 	async execute(interaction) {
-		await interaction.reply({ files: ['./assets/shadow_milk_plush.png'] });
+		files = [
+			'./assets/shadow_milk_plush.png',
+			'./assets/shadow_milk_sing.png',
+			'./assets/shadow_milk_laugh.png',
+		];
+		await interaction.reply({ files: [files[getRandInt(0, files.length - 1)]] });
 	},
 };
